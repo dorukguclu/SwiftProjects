c6c2b9c8bcb93310d07d84eb72d4264f161b615d
dorukguclu <doruk.guclu@hisarschool.k12.tr>
2024-09-12 10:47:57 +0300

    Update Contents.swift

--- isim yazdırma copy.playground/Contents.swift
+++ isim yazdırma copy.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
 // İsminizi 10 defa yazdıran for döngüsü
-for _ in 1...10 {
-    print("Doruk")
+for _ in 1...10 {                    // 10 kere
+    print("Doruk")                //Doruk adını yazdır
 }
