28ff4fdf4492508f96c86bcd2678455404384bfe
dorukguclu <doruk.guclu@hisarschool.k12.tr>
2024-09-12 10:43:49 +0300

    Update Contents.swift

--- hava durumu copy.playground/Contents.swift
+++ hava durumu copy.playground/Contents.swift
@@ -1,13 +1,13 @@
 import UIKit
 
-let weather = "rain"
+let weather = "rain"                //Setting the weather as rain
 
-if weather == "rain" {
-    print("Şemsiye almalısınız.")
+if weather == "rain" {                  
+    print("Şemsiye almalısınız.")              //If the weather is rain, prints this
 } else if weather == "snow" {
-    print("Atkı almalısınız.")
-} else if weather == "sunny" {
-    print("Güneş gözlüğü almalısınız.")
+    print("Atkı almalısınız.")                //If it is snow, prints this
+} else if weather == "sunny" {                
+    print("Güneş gözlüğü almalısınız.")            //If it is sunny, prints this
 } else {
-    print("Özel bir tavsiyem yok.")
+    print("Özel bir tavsiyem yok.")                //Else, prints this
 }
