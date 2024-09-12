d78ebfe5836165ab77a9f07550f290dde94b960a
dorukguclu <doruk.guclu@hisarschool.k12.tr>
2024-09-12 10:49:14 +0300

    Add files via upload

---
+++ fonksiyonlar copy.playground/Contents.swift
@@ -0,0 +1,8 @@
+func dikdortgenAlani(uzunluk: Double, genislik: Double) -> Double {
+    return uzunluk * genislik
+}
+
+// Örnek kullanım
+let alan = dikdortgenAlani(uzunluk: 10.0, genislik: 5.0)
+print("Dikdörtgenin alanı: \(alan)")
+
