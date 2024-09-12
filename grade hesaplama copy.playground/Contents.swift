4fe5cb90bffda284c53bbcf7814c54a75f130074
dorukguclu <doruk.guclu@hisarschool.k12.tr>
2024-09-12 10:47:00 +0300

    Add files via upload

---
+++ grade hesaplama copy.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// İki not tanımlaması
+let not1 = 90
+let not2 = 97
+
+// Ortalamayı hesapla
+let ortalama = (not1 + not2) / 2
+
+// Karar yapısı
+if ortalama >= 50 {
+    print("Geçtiniz. Ortalamanız: \(ortalama)")
+} else {
+    print("Kaldınız. Ortalamanız: \(ortalama)")
+}
+
