2baac5255964ab5e0d607c9a46fdf46af91adbc1
dorukguclu <doruk.guclu@hisarschool.k12.tr>
2024-09-12 10:42:39 +0300

    Add files via upload

---
+++ hava durumu copy.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let weather = "rain"
+
+if weather == "rain" {
+    print("Şemsiye almalısınız.")
+} else if weather == "snow" {
+    print("Atkı almalısınız.")
+} else if weather == "sunny" {
+    print("Güneş gözlüğü almalısınız.")
+} else {
+    print("Özel bir tavsiyem yok.")
+}
