1f870ca3e6ae28216d378da7641b7c2d8ff4fff8
dorukguclu <doruk.guclu@hisarschool.k12.tr>
2024-09-14 13:52:32 +0300

    Update Contents.swift

--- week1 func copy.playground/Contents.swift
+++ week1 func copy.playground/Contents.swift
@@ -1,9 +1,9 @@
 import UIKit
 import Foundation
 
-func guncelTarihVeSaat() {
-    let tarih = Date()
-    print("Şu anki tarih ve saat: \(tarih)")
+func guncelTarihVeSaat() {                //Güncel tarih ve saat fonksiyonunu yarat
+    let tarih = Date()                    
+    print("Şu anki tarih ve saat: \(tarih)")                ////Tarihi Date olarak ayarla ve yazdır
 }
 
 guncelTarihVeSaat()
@@ -12,19 +12,20 @@ guncelTarihVeSaat()
 
 func rastgeleSayi() -> Int {
     let sayi = Int.random(in:1...100)
-    return sayi
+    return sayi                                    //Sayı'yı 1'den 100e kadar rastgele seç 
 }
 let sayi = rastgeleSayi()
-print("Rastgele seçilen sayı: \(sayi)")
+print("Rastgele seçilen sayı: \(sayi)")                //Rastgele seçtiğin sayıyı sayı sabitine ata ve yazdır
+
 
 
 func kitapBilgileri() -> (String, Int) {
     let kitapAdi = "Swift Progamlama"
     let sayfaSayisi = 350
-    return (kitapAdi, sayfaSayisi)
+    return (kitapAdi, sayfaSayisi)                                //Kitap bilgilerini gir, string ve integer olarak ayır
 }
 
 let (adi, sayfa) = kitapBilgileri()
 
 print("Kitap adı: \(adi)")
-print("Sayfa sayısı: \(sayfa)")
+print("Sayfa sayısı: \(sayfa)")                        //Sırasıyla kitap adını ve sayısını yazdır
